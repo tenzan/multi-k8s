dd8064b2f4a70f5a860e67fc0f8d7cc6ebbaff48
Askar Karasaev <askar.karasaev@gmail.com>
2020-08-05 19:34:00 +0900

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t tenzan/multi-client:latest -t tenzan/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t tenzan/multi-server:latest -t tenzan/multi-server:$SHA-f ./server/Dockerfile ./server
+docker build -t tenzan/multi-worker:latest -t tenzan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push tenzan/multi-client:latest
+docker push tenzan/multi-server:latest
+docker push tenzan/multi-worker:latest
+
+docker push tenzan/multi-client:$SHA
+docker push tenzan/multi-server:$SHA
+docker push tenzan/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=tenzan/multi-server:$SHA
+kubectl set image deployments/client-deployment client=tenzan/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=tenzan/multi-worker:$SHA
